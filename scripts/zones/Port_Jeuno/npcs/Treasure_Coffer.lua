fec8fec204db2ee4c56fbbe7bd25416020889d11
warlord1352 <steve98890@yahoo.com>
2018-04-18 00:39:37 -0700

    add NPC "store" for AF3 items

--- scripts/zones/Port_Jeuno/npcs/Treasure_Coffer.lua
+++ scripts/zones/Port_Jeuno/npcs/Treasure_Coffer.lua
@@ -1,19 +1,26 @@
 -----------------------------------
 -- Area: Port Jeuno
 --  NPC: Treasure Coffer
--- !pos  -52 0 -11 246
+-- AF3 Merchant NPC
 -----------------------------------
 package.loaded["scripts/zones/Port_Jeuno/TextIDs"] = nil;
 -----------------------------------
 require("scripts/zones/Port_Jeuno/TextIDs");
------------------------------------
+require("scripts/globals/shop");
+
 
 function onTrade(player,npc,trade)
 end;
 
 function onTrigger(player,npc)
 
-    player:messageSpecial(CHEST_IS_EMPTY);
+    player:showText(npc,GEKKO_SHOP_DIALOG);
+    local stock =
+    {
+        3195,100000,     -- Raiders seal: Feet
+        12093,100000      -- Raiders Feet
+    }
+    showShop(player, STATIC, stock);
 end;
 
 function onEventUpdate(player,csid,option)
