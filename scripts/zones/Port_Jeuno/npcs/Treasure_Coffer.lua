e0ea89025cea24f4f85a88d4433d2711a7df9f2b
warlord1352 <steve98890@yahoo.com>
2018-04-25 20:00:57 -0700

    updated treasure coffer af3

--- scripts/zones/Port_Jeuno/npcs/Treasure_Coffer.lua
+++ scripts/zones/Port_Jeuno/npcs/Treasure_Coffer.lua
@@ -14,14 +14,32 @@ end;
 
 function onTrigger(player,npc)
 
-    player:showText(npc,GEKKO_SHOP_DIALOG);
-    local stock =
-    {
-        3195,100000,     -- Raiders seal: Feet
-        12093,100000      -- Raiders Feet
-    }
-    showShop(player, STATIC, stock);
+	if (player:getMainJob() == JOBS.THF) then
+
+		player:showText(npc,GEKKO_SHOP_DIALOG);
+		local stock_1 =
+		{
+			12073,100000;
+			12093,100000      -- Raiders Feet
+		}
+		showShop(player, STATIC, stock_1);
+		
+	elseif (player:getMainJob() == JOBS.RDM) then
+	
+		player:showText(npc,TREASURE_COFFER_DIALOG);
+		local stock_2 =
+		{
+			12012,100000;      -- Estoqueurs Head
+			12032,100000;		--Estoqueurs Body
+			12092,100000;		--Estoqueur feet
+			12052,100000;		--Estoqueur hands
+			12072,100000		--Estoqueur legs
+		}
+		showShop(player, STATIC, stock_2);
+		
+	end;
 end;
+
 
 function onEventUpdate(player,csid,option)
     -- printf("CSID: %u",csid);
